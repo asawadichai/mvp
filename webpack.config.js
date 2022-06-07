bbce4505b956e1604c668eeaabf249ab6d4a453c
asawadichai <sawadichai.andrew@gmail.com>
2022-06-06 21:59:35 -0700

    Setting up environment with babel and webpack.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client/src/index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+  },
+  module: {
+    rules: [{test: /\.jsx?/, exclude: /node_modules/, use: {loader: "babel-loader"}}]
+  }
+};
